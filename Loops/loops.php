7acf056c971b2e67c443b86f792752d6fd8fdced
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-15 12:34:30 +0100

    Create loops.php

---
+++ Loops/loops.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops</title>
+</head>
+<body>
+    <?php
+
+    // $x =0;
+
+    // while ($x <= 10) {
+    //     echo '</br>'. $x;
+    //     $x++;
+    // }
+
+    $a= 1;
+    do {
+       echo "</br>" .$a;
+       $a++;
+    } while ($a <= 10);
+
+
+    // for ($i=0; $i <4 ; $i++) { 
+    //    echo '</br> a';
+    // }
+
+
+
+?>
+</body>
+</html>
