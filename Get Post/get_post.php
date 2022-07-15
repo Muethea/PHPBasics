cd84a1d8e6df1923bd587c3992e63e21b7dec2e4
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-15 13:01:56 +0100

    get and post

---
+++ Get Post/get_post.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get and Post</title>
+</head>
+<body>
+
+<form action="data.php" method="post">
+    <input type="password" name="password" id="" >
+    <input type="submit" value="submit">
+</form>
+    <?php
+
+
+?>
+</body>
+</html>
