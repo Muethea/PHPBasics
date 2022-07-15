a9424ca1c4886fc2aafa3b7ea7f6bb59aa7d6c28
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-15 13:51:41 +0100

    Create implodeExplode.php

---
+++ ImplodeExplode/implodeExplode.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>
+
+<?php
+
+$str = "My name is Fernando";
+$exp = explode(" ", $str);
+
+
+echo "<pre>";
+
+print_r($exp);
+
+$produts = array(
+    'Carro',
+    'Bike'
+);
+
+$imp = implode(" ", $produts);
+echo "<pre>";
+print_r($imp);
+?>
+</h2>
+</body>
+</html>
