43d93ccd134c97348a4fc40948a821a150bbd2f7
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-15 09:44:39 +0100

    datatype

---
+++ datatype.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Type</title>
+</head>
+<body>
+    <h2>Data type</h2>
+
+    <?php
+ 
+ $name = "Fernando dos Santos";
+
+ $age =24;
+
+ if ($age > 18){
+    echo 'Vote';
+ }else{
+    echo 'Not Vote';
+ }
+ echo "</br>";
+
+echo 'Your name is ', $name, ' your age is ',$age;
+echo '</br>';
+
+var_dump($name);
+?>
+</body>
+</html>
