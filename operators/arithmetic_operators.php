b482bed8252b775dad953fc3f995db6207c39b86
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-15 10:57:55 +0100

    Create arithmetic_operators.php

---
+++ operators/arithmetic_operators.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arithmetic Operators</title>
+</head>
+<body>
+    <?php
+
+$number1 = 20;
+$number2 = 12;
+/*
+$ad = $number1 + $number2;
+
+$min = $number1 - $number2;
+
+$mul = $number1 * $number2;
+
+$div = $number1 / $number2;
+
+$mod = $number1 % $number2;
+echo 'n1 = ', $number1, ' n2 = ', $number2, '</br>';
+echo 'Ad = ', $ad,'</br>', 'min = ', $min, '</br>';
+echo 'Mul = ', $mul,'</br>', 'mod = ', $mod;
+
+*/
+
+if ($number1 == $number2){
+    echo ' number 1 is equal to number 2';
+}
+else{
+    echo $number1, ' is not equal to ', $number2;
+}
+
+
+?>
+   
+</body>
+</html>
