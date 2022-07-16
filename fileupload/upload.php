6e3511782fb13fee70222dce307467ff29223386
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-07-16 03:04:41 +0100

    Update upload.php

--- fileupload/upload.php
+++ fileupload/upload.php
@@ -4,12 +4,27 @@
 if (isset($_POST['submit'])) {
     $f_name =  $_FILES['file']['name'];
     $f_tmp = $_FILES['file']['tmp_name'];
-    $store = "../upload/".$f_name;
-if (
-    move_uploaded_file($f_tmp, $store)) {
-    echo 'file Uploded';
-}
 
+    $f_size =$_FILES['file']['size'];
+    $f_extension = explode('.',$f_name);
+    $f_extension = strtolower(end($f_extension));
+    $f_newfile = uniqid().'.'.$f_extension;
+
+    $store = "../upload/".$f_newfile;
+
+  if ($f_extension =='jpg' || $f_extension =="png" ) {
+    if ($f_size>= 1000000) {
+        echo 'Max file should be 1Mb';
+    } else {
+        if (move_uploaded_file($f_tmp, $store)) {
+            echo 'File Uploded';
+        }
+    }
+    
+  } else {
+    echo 'This is pdf';
+  }
+   
 }
 
 ?>
